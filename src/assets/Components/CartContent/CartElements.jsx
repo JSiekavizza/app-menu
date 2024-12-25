3c20e1489a5c92eace9fc5dfb54265cf22a6f187
JSiekavizza <132721255+JSiekavizza@users.noreply.github.com>
2024-12-25 17:23:47 -0500

    Update CartElements.jsx

--- src/assets/Components/CartContent/CartElements.jsx
+++ src/assets/Components/CartContent/CartElements.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useContext } from "react";
-import { dataContext } from "../Context/DataContext";
+import { dataContext } from "../Context/dataContext";
 
 import CartItemCounter from "./CartItemCounter";
 import { AiOutlineDelete } from "react-icons/ai";
